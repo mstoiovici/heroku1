3a1a9bf258b02d3503371066c5890e13e923a2e9
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2019-01-21 23:51:59 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 23:44:21 2019
+
+@author: maria
+"""
+
+from flask import Flask,render_template
+app=Flask("MyApp")
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="My Fourth App On Heroku", **locals())
+
+app.run(debug=True)
